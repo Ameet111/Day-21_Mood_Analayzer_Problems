8d6e79c282d45886822f36af7ed6da60dd1bb0b5
Ameet Jadhav <ammugs11@gmail.com>
2022-10-02 15:12:19 +0530

    added using method analayser sad else happy

--- src/main/java/com/bridgelab/junit/MoodAnalyzer.java
+++ src/main/java/com/bridgelab/junit/MoodAnalyzer.java
@@ -1,5 +1,15 @@
 package com.bridgelab.junit;
 
 public class MoodAnalyzer {
+	
+	public MoodAnalyzer() {
+	}
+
+	public String MoodAnalyzer(String message) {
+		if (message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
 
 }
