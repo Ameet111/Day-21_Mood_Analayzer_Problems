ae98be6e033cf9d758bc67402afb8eebd1a488bb
Ameet Jadhav <ammugs11@gmail.com>
2022-10-02 15:50:47 +0530

    added using method no params return happy

---
+++ src/main/java/com/bridgelabmoodanalyzer1/MoodAnalyzer.java
@@ -0,0 +1,29 @@
+package com.bridgelabmoodanalyzer1;
+
+
+/**
+ * Purpose - “I am in Happy Mood” message Should Return Happy
+ *
+ */
+
+public class MoodAnalyzer {
+	
+	private String message;
+
+	// Default Constructor
+	public MoodAnalyzer() {
+	}
+
+	// Parameterized constructor
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (this.message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
+
+}
