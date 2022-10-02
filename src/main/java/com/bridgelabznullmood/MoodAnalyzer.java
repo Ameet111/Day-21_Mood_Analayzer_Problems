3ec83fbc778b1a758b6fdb6539ab790ff503278a
Ameet Jadhav <ammugs11@gmail.com>
2022-10-02 15:58:13 +0530

    added using method null mood return happy

---
+++ src/main/java/com/bridgelabznullmood/MoodAnalyzer.java
@@ -0,0 +1,34 @@
+package com.bridgelabznullmood;
+
+/**
+ * Purpose - Null Mood Should Return Happy
+ *
+ */
+
+public class MoodAnalyzer {
+	
+	private String message;
+
+	// Default Constructor
+	public MoodAnalyzer() {
+	}
+
+	// Parameterized constructor
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	// Handling Exception
+	public String analyseMood() {
+		try {
+
+			if (this.message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
+	}
+
+}
